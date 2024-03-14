10553c9899b929ac31da13dcb8039a9632b1ce02
tricker7272 <87934416+tricker7272@users.noreply.github.com>
2024-03-14 13:52:16 +0700

    Update tlc.cmd

--- tlc.cmd
+++ tlc.cmd
@@ -1,5 +1,1 @@
-curl -o "C:\Users\Administrator\Desktop\AutoTangLikeCheo.zip" "https://drive.google.com/uc?id=159CmpmXL54hX-sFxcoGMqrUQaMmLFRoq&export=download&confirm=t&uuid=3ce136a4-c505-4b75-a4c2-dfff56f31462"
-cd 'C:\Users\\Administrator\Desktop'
-expand AutoTangLikeCheo.zip -F:* .
-cd 'C:\Users\\Administrator\Desktop\AutoTangLikeCheo'
-Start 'AutoTangLikeCheo.exe'
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://drive.usercontent.google.com/download?id=159CmpmXL54hX-sFxcoGMqrUQaMmLFRoq&export=download&confirm=t&uuid=3ce136a4-c505-4b75-a4c2-dfff56f31462', 'C:\Users\Administrator\Desktop\AutoTangLikeCheo.zip'); Expand-Archive -Path 'C:\Users\Administrator\Desktop\AutoTangLikeCheo.zip' -DestinationPath 'C:\Users\Administrator\Desktop'; cd 'C:\Users\Administrator\Desktop\AutoTangLikeCheo'; Start-Process -FilePath 'AutoTangLikeCheo.exe'"

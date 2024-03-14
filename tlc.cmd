538ebfeab57bb72ab3e56e0383589851de8e0b81
tricker7272 <87934416+tricker7272@users.noreply.github.com>
2024-03-14 13:47:42 +0700

    Add files via upload

---
+++ tlc.cmd
@@ -0,0 +1,5 @@
+curl -o "C:\Users\Administrator\Desktop\AutoTangLikeCheo.zip" "https://drive.google.com/uc?id=159CmpmXL54hX-sFxcoGMqrUQaMmLFRoq&export=download&confirm=t&uuid=3ce136a4-c505-4b75-a4c2-dfff56f31462"
+cd 'C:\Users\\Administrator\Desktop'
+expand AutoTangLikeCheo.zip -F:* .
+cd 'C:\Users\\Administrator\Desktop\AutoTangLikeCheo'
+Start 'AutoTangLikeCheo.exe'
